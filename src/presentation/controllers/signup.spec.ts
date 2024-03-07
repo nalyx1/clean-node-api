7bde6e0631f1a7fb058db106051c89f060bd6630
nalyx <joojrodrigues.dev@outlook.com>
2024-03-06 21:09:27 -0300

    test: ensure SignUpController returns 400 if no name if provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup';
+
+describe('SignUp Controller', () => {
+  test('should return 400 if no name is provided', async () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+  });
+});
