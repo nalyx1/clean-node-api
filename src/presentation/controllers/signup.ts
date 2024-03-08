9d7d4c76daf984426dde6050c6d367935ba64d02
nalyx <joojrodrigues.dev@outlook.com>
2024-03-07 21:21:32 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,9 +6,13 @@ import {
 } from '../protocols';
 import { MissingParamError, InvalidParamError } from '../errors';
 import { badRequest, serverError } from '../helpers/http-helper';
+import { AddAccount } from '../../domain/usecases/add-account';
 
 export class SignUpController implements Controller {
-  constructor(private readonly emailValidator: EmailValidator) {}
+  constructor(
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount
+  ) {}
   handle(httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = [
@@ -34,6 +38,12 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'));
       }
+
+      this.addAccount.add({
+        name,
+        email,
+        password
+      });
 
       return {
         statusCode: 200,
